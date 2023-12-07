3c867ce4a93a8a12827deed00d7ebbfdf096aa24
Jeffrey <jaysoncheng999@gmail.com>
2023-12-08 02:21:10 +0800

    create registration form, add form validation check

--- app/src/main/java/com/example/expensemanagerapp/MainActivity.java
+++ app/src/main/java/com/example/expensemanagerapp/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.expensemanagerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button btnLogin;
+    private TextView mForgetPassword;
+    private TextView mSignupHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginDetails();
+    }
+
+    private void loginDetails() {
+        mEmail = findViewById(R.id.email_login);
+        mPassword = findViewById(R.id.password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        mForgetPassword = findViewById(R.id.forget_password);
+        mSignupHere =  findViewById(R.id.signup_reg);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email Required..");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password Required..");
+                    return;
+                }
+            }
+        });
+
+        mSignupHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
+            }
+        });
+
+//        Reset password activity..
+        mForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ResetActivity.class));
+            }
+        });
+
     }
 }
